58caa5751cd79e222a6d91f24696417aaf850a87
Ookami <ookamikenrou@gmail.com>
2015-12-08 21:02:44 +0100

    Add package info and bump version

--- mix.exs
+++ mix.exs
@@ -3,9 +3,11 @@ defmodule YURI.Mixfile do
 
   def project do
     [app: :yuri,
-     version: "0.7.0",
+     description: "Simple struct for URI representation.",
+     version: "1.0.0",
      elixir: "~> 1.0",
      deps: deps,
+     package: package,
      name: "YURI",
      source_url: "https://github.com/kemonomachi/yuri",
      docs: [extras: ["README.md": [path: "README", title: "README"]],
@@ -15,6 +17,12 @@ defmodule YURI.Mixfile do
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev}]
+  end
+
+  defp package do
+    [maintainers: ["Ookami"],
+     licenses: ["WTFPL"],
+     links: %{"GitHub" => "https://github.com/kemonomachi/yuri"}]
   end
 end
 
